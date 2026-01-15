efd43e9e0d5ea6739921fe45a8ece7b4381dba23
Srishti B S <srishtisrivatsa4@gmail.com>
2026-01-15 23:54:18 +0530

    Delete verify_password.py

--- verify_password.py
+++
@@ -1,9 +0,0 @@
-spwd="PU@123"
-in_pwd=input("Enter a password")
-age=int(input("Enter the age"))
-if age<18:
-    print("Access denied")
-elif spwd==in_pwd:
-    print("Login successfully")
-else:
-    print("Incorrect password")
