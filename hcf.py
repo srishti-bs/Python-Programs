2325bc2f5c68d8ca95954579faba52bf51fd64d3
Srishti B S <srishtisrivatsa4@gmail.com>
2026-02-03 23:35:52 +0530

    Update hcf.py

--- hcf.py
+++ hcf.py
@@ -3,13 +3,14 @@
 def compute_hcf(x, y):
 # choose the smaller number
 if x > y:
-smaller = y
+  smaller = y
 else:
-smaller = x
+  smaller = x
 for i in range(1, smaller+1):
-if((x % i == 0) and (y % i == 0)):
-hcf = i 
-return hcf
+  if((x % i == 0) and (y % i == 0)):
+      hcf = i 
+      return hcf
 num1 = int(input('Enter the number: '))
 num2 = int(input('Enter the number: '))
+
 print("The H.C.F. is", compute_hcf(num1, num2))
